e4cd1f8c8438d2616413ebb7d19c29ea35e66ee4
Chriszlade <christopheragbozo434@gmail.com>
2025-11-02 18:47:09 +0000

    Add project files.

---
+++ GA_1/GA_1.cpp
@@ -0,0 +1,22 @@
+// GA_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+
+
+
+
+
+
+    cout << "Hello" << endl;
+}
+
+
